7570cc40f4cb929361487b371354bd8b42af84b2
Robbi <codewithwafik@gmail.com>
2023-09-17 19:54:46 +0700

    Initial commit

---
+++ src/test/java/com/doodle/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doodle.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
