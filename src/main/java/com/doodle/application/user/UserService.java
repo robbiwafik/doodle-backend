902e73308180097ce998df15e767408378c8b291
Robbi <codewithwafik@gmail.com>
2023-09-18 20:18:42 +0700

    Create User service

---
+++ src/main/java/com/doodle/application/user/UserService.java
@@ -0,0 +1,24 @@
+package com.doodle.application.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with the given username does not exist."));
+    }
+}
