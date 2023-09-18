bae0782b79b4bd0a3ae923548ae90c86bd7843fb
Robbi <codewithwafik@gmail.com>
2023-09-18 21:49:33 +0700

    Register Bean of type SecurityFilterChain in SecurityConfig class

--- src/main/java/com/doodle/application/security/SecurityConfig.java
+++ src/main/java/com/doodle/application/security/SecurityConfig.java
@@ -6,9 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -23,6 +25,18 @@ public class SecurityConfig {
         authenticationProvider.setPasswordEncoder(getPasswordEncoder());
 
         return authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
     }
 
     @Bean
