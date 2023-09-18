26629e94feb9ebd65b021536d98fda62554ece58
Robbi wafik <robbiwafik31@gmail.com>
2023-09-19 00:24:17 +0700

    Add NotNull validation to every field in AdminUserDTO

--- src/main/java/com/doodle/application/user/AdminUserDTO.java
+++ src/main/java/com/doodle/application/user/AdminUserDTO.java
@@ -3,6 +3,7 @@ package com.doodle.application.user;
 
 import com.doodle.application.validation.annotations.UniqueUsername;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,17 +14,21 @@ import org.hibernate.validator.constraints.Length;
 @NoArgsConstructor
 public class AdminUserDTO {
     @NotBlank(message = "First name is required.")
+    @NotNull
     private String firstName;
 
     @NotBlank(message = "Last name is required.")
+    @NotNull
     private String lastName;
 
     @NotBlank(message = "Username is required")
+    @NotNull
     @Length(min = 8, max = 30)
     @UniqueUsername
     private String username;
 
     @NotBlank(message = "Password is required")
+    @NotNull
     @Length(min = 8, max = 30)
     private String password;
 }
