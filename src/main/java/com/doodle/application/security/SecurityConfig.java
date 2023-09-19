26d4c3af49803e304e213b8c726cf48abb4e77b2
Robbi wafik <robbiwafik31@gmail.com>
2023-09-19 14:05:04 +0700

    Register Bean of type AuthenticationManager in SecurityConfig class

--- src/main/java/com/doodle/application/security/SecurityConfig.java
+++ src/main/java/com/doodle/application/security/SecurityConfig.java
@@ -4,8 +4,10 @@ import com.doodle.application.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,6 +19,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Autowired
     UserService userService;
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public AuthenticationProvider getAuthenticationProvider() {
