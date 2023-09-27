f34e0d96712584370203cc6e32c174086e73a17a
Robbi wafik <robbiwafik31@gmail.com>
2023-09-27 20:58:07 +0700

    Create CartRepository

---
+++ src/main/java/com/doodle/application/cart/CartRepository.java
@@ -0,0 +1,13 @@
+package com.doodle.application.cart;
+
+import com.doodle.application.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, String> {
+    Optional<Cart> findByCustomer(User customer);
+}
