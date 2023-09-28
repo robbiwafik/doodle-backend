ad7bd244440197685c6fe2eab2688d22eaee0773
Robbi wafik <robbiwafik31@gmail.com>
2023-09-28 08:37:23 +0700

    Create CartItem entity

---
+++ src/main/java/com/doodle/application/cartItem/CartItem.java
@@ -0,0 +1,32 @@
+package com.doodle.application.cartItem;
+
+import com.doodle.application.cart.Cart;
+import com.doodle.application.product.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+}
