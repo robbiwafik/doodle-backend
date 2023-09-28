9d2d8bce18035e12415561b467db57af004b571a
Robbi wafik <robbiwafik31@gmail.com>
2023-09-28 08:42:00 +0700

    Create CartItemDTO

---
+++ src/main/java/com/doodle/application/validation/classes/ValidProductIdValidator.java
@@ -0,0 +1,25 @@
+package com.doodle.application.validation.classes;
+
+import com.doodle.application.product.ProductService;
+import com.doodle.application.validation.annotations.ValidProductId;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class ValidProductIdValidator implements ConstraintValidator<ValidProductId, Integer> {
+    @Autowired
+    private ProductService productService;
+
+    @Override
+    public boolean isValid(Integer id, ConstraintValidatorContext constraintValidatorContext) {
+        try {
+            productService.getProduct(id);
+            return true;
+        }
+        catch (EntityNotFoundException exception) {
+            return false;
+        }
+    }
+}
