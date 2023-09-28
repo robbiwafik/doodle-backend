62b84f22abfc280faca5512abf78b7f9f53d4a74
Robbi wafik <robbiwafik31@gmail.com>
2023-09-28 22:50:50 +0700

    Handle anonymous user error when getting current authenticated user

--- src/main/java/com/doodle/application/auth/AuthenticationUtil.java
+++ src/main/java/com/doodle/application/auth/AuthenticationUtil.java
@@ -1,11 +1,17 @@
 package com.doodle.application.auth;
 
 import com.doodle.application.user.User;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 
 public class AuthenticationUtil {
-    public static User getCurrentAuthenticatedUser() {
-        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    public static User getCurrentAuthenticatedUser() throws AuthenticationException {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal == "anonymousUser")
+            throw new AuthenticationCredentialsNotFoundException("Missing authorization token.");
+
+        return (User) principal;
     }
 }
